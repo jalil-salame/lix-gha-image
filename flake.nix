d7b3638eb6e3bc3b3937aee7afb26622f3f40805
Jalil David Salamé Messina <jalil.salame@gmail.com>
2024-09-10 09:01:21 +0200

    fix(ci): invalid check command

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs = "nixpkgs/nixos-unstable";
+    nixpkgs.url = "nixpkgs/nixos-unstable";
     lix-module = {
       url = "https://git.lix.systems/lix-project/nixos-module/archive/2.91.0.tar.gz";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,38 +9,40 @@
 
   outputs =
     { nixpkgs, lix-module, ... }:
+    let
+      inherit (nixpkgs) lib;
+      systems = [ "x86_64-linux" ];
+      nixpkgsFor = lib.genAttrs systems (
+        system:
+        import nixpkgs {
+          inherit system;
+          overlays = [ lix-module.overlays.lixFromNixpkgs ];
+        }
+      );
+      forEachSystem = f: lib.genAttrs systems (system: f nixpkgsFor.${system});
+    in
     {
-      nixosConfigurations.lix-gha = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-
-        modules = [
-          (
-            { pkgs, ... }:
-            {
-              nix = {
-                # Pin nixpkgs
-                registry.nixpkgs.flake = nixpkgs;
-                # Enable flakes
-                settings.experimental-features = [
-                  "nix-command"
-                  "flakes"
-                ];
-              };
-              # Required by GitHub actions
-              environment.systemPackages = with pkgs; [
-                git
-                bash
-                coreutils
-                git
-                gnutar
-                gzip
-                xz
-                nodejs_20
-              ];
-            }
-          )
-          lix-module.nixosModules.default
-        ];
-      };
+      packages = forEachSystem (pkgs: {
+        lix-gha-image = pkgs.dockerTools.buildImage {
+          name = "lix-gha-image";
+          tag = "latest";
+          fromImage = "ghcr.io/lix-project/lix";
+          copyToRoot = pkgs.buildEnv {
+            name = "image-root";
+            paths = with pkgs; [
+              bash
+              coreutils
+              git
+              gnutar
+              gzip
+              nodejs_20
+              # Useful for nix projects
+              nix
+              nix-fast-build
+            ];
+            pathsToLink = [ "/bin" ];
+          };
+        };
+      });
     };
 }
